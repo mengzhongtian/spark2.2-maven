7e366fd73570f4aa4dda1dda8e529f6e8c7f8b4a
mike <mengzhongtian@126.com>
2017-11-20 14:14:08 +0800

    创建mooc模块

--- mooc/src/main/java/kafka/MyKafkaProducer.java
+++ mooc/src/main/java/kafka/MyKafkaProducer.java
@@ -27,7 +27,7 @@ public class MyKafkaProducer {
         */
 
 
-        Properties props = new Properties();
+        /*Properties props = new Properties();
         props.put("bootstrap.servers", "192.168.204.3:9092");
         props.put("acks", "all");
         props.put("retries", 0);
@@ -47,7 +47,27 @@ public class MyKafkaProducer {
                 e.printStackTrace();
             }
 
+        }*/
+
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.204.3:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        for (int i = 0; i < 100; i++) {
+
+            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+            System.out.println(i);
+            producer.flush();
         }
+
+        producer.close();
 
     }
 
