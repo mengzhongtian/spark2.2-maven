3831fd3c4d16bc1d0860dfc89aa008cf720d7f55
mengao <meng_ao@cdv.com>
2017-11-20 15:02:13 +0800

    复制了老师的代码

---
+++ mooc/src/main/java/mooc/KafkaProperties.java
@@ -0,0 +1,16 @@
+package mooc;
+
+/**
+ * Kafka常用配置文件
+ */
+public class KafkaProperties {
+
+    public static final String ZK = "192.168.204.3:2181";
+
+    public static final String TOPIC = "test";
+
+    public static final String BROKER_LIST = "192.168.204.3:9092";
+
+    public static final String GROUP_ID = "test_group1";
+
+}
