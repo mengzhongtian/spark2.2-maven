d0a152443f62f7419b644410fdeaa3cb343eb4dd
mengao <meng_ao@cdv.com>
2017-11-22 19:03:58 +0800

    添加SparkStreaming整合Flume成功，并且添加注释

--- mooc/src/main/scala/FlumeKafka2Streaming/FlumePush2Streaming.scala
+++ mooc/src/main/scala/FlumeKafka2Streaming/FlumePush2Streaming.scala
@@ -17,6 +17,39 @@ object FlumePush2Streaming {
     *3.flumeStream.map(x => new String(x.event.getBody.array()).trim)
     * 理解这部操作
     *
+    * 4.打包
+    * (1)把依赖打入jar包中提交上去
+    * 要添加的jar：
+    * flume-ng-sdk-1.6.0.jar
+    * spark-streaming-flume_2.11-2.2.0.jar
+    * 打包：
+    * artifacts -> extracted directory 选择这两个jar包。
+    *
+    * 打包遇到的问题，如果全打进去，会报错：找不到主类：原因是要把linux环境中已经存在的依赖从打的jar包中移除
+    *
+    * (2)把依赖拷贝到linux上然后通过--jars a.jar,b.jar
+    * 逗号分隔的方式
+    *
+    * 5.提交：spark-submit --class FlumeKafka2Streaming.FlumePush2Streaming mooc.jar
+    *
+    * 6.提交顺序：
+    * (1)先提交spark-submit
+    * spark-submit --class FlumeKafka2Streaming.FlumePush2Streaming mooc.jar
+    * (2)然后启动flume
+    * bin/flume-ng agent  \
+    * --name simple-agent   \
+    * --conf conf    \
+    * --conf-file conf/flume_push_streaming.conf  \
+    * -Dflume.root.logger=INFO,console
+    *
+    * (3)然后启动telnet：
+    * telnet localhost 4444
+    *
+    * 注意：----------------------------------------------------------------------------
+    * 区分telnet和netcat：nc -lk 4444
+    * telnet localhost 4444
+    *
+    *
     */
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[2]").setAppName("FlumePush2Streaming")
